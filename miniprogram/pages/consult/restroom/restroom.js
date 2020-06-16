ca9fbdc671d15273e15218051b61b0d147e04307
lilei <997797281@qq.com>
2020-06-16 18:37:15 +0800

    123

---
+++ miniprogram/pages/consult/restroom/restroom.js
@@ -0,0 +1,137 @@
+// pages/pyq/pyq.js
+Page({
+  
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    percent: 50,
+    value: 80,
+    gradientColor: {
+      '0%': '#509ffb',
+      '50%': '#40e5ff',
+      '100%': '#509ffb',
+
+    },
+  },
+
+  goRestroom:function(){
+    //连接socket，代表进入房间
+    wx.connectSocket({
+      url: 'ws://192.168.1.133:5555/websocket/1234/121',
+      method: 'GET',
+      success: function(){
+        isConnect: true
+        console.log("连接成功...")
+      },
+      fail: function(){
+        isConnect: false
+        console.log("连接失败...")
+      }
+    });
+    wx.onSocketOpen(function (res) {
+      console.log('WebSocket连接已打开！')
+    });
+ 
+    wx.onSocketError(function (res) {
+      console.log('WebSocket连接打开失败，请检查！')
+    });
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var that = this;
+    wx.onSocketMessage(function(res){
+      
+      var json = JSON.parse(res.data); 
+      console.log(json)
+     
+      if(json.msg == "技师进入了房间"){
+        console.log("-----------------success");
+        that.setData({
+          teacher_icon: json.data.icon
+        })
+      }
+      if(json.msg == "技师离开了房间"){
+        console.log("-----------------success22222");
+        that.setData({
+          teacher_icon: ""
+        })
+      }
+      if(json.msg == "用户进入了房间"){
+        console.log("-----------------success");
+        that.setData({
+          user_icon: json.data.icon
+        })
+      }
+      if(json.msg == "用户离开了房间"){
+        console.log("-----------------success22222");
+        that.setData({
+          user_icon: ""
+        })
+      }
+      if(json.msg == "获取到用户信息"){
+        console.log("-----------------success22222");
+        that.setData({
+          user_icon: json.data.icon
+        })
+      }
+      if(json.msg == "获取到技师信息"){
+        console.log("-----------------success22222");
+        that.setData({
+          teacher_icon: json.data.icon
+        })
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
