e671adfae22226dbf27c96470763f1fbddf0c9bb
liuyuwei <1349541630@qq.com>
2021-01-03 22:37:17 +0800

    咨询技师相关界面

--- miniprogram/pages/technician/technicianDetail/technicianDetail.js
+++ miniprogram/pages/technician/technicianDetail/technicianDetail.js
@@ -9,8 +9,9 @@ Page({
     technicianData:{},
     imgtxtSolvePrice:0,
     videoSolvePrice:0,
-    teacherComments:[]
-
+    teacherComments:[],
+    show:false,
+    phone:''
   },
 
   /**
@@ -21,6 +22,55 @@ Page({
     this.getTeacherComment(this.data.teacherId,1,10);
     this.getTechDetail(this.data.teacherId);
 
+  },
+  onPay: function () {
+    var me = this;
+    var myreg = /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1})|(17[0-9]{1}))+\d{8})$/;
+    if (!myreg.test(this.data.phone)){
+      wx.showToast({
+        title: '手机号格式不正确',
+        icon: 'none'
+      })
+      return ;
+    }
+    var url = 'consult/payment/videoPayment?teacherId=' + this.data.teacherId + '&payMoney=' + this.data.technicianData.videoSolvePrice;
+    app.wxRequest('get', url, {}, function (data) {
+      if (data.data.status == 200) {
+        var payData = data.data.data;
+        console.log();
+        wx.requestPayment({
+          timeStamp: payData.timeStamp,
+          nonceStr: payData.nonceStr,
+          package: payData.package,
+          signType: payData.signType,
+          paySign: payData.paySign,
+          success(res) {
+            wx.navigateTo({
+              url: '/pages/technician/paySuccess/paySuccess?payMoney=' + this.data.technicianData.videoSolvePrice
+            })
+          },
+          fail(res) {
+            console.warn(res)
+          }
+        })
+      }
+    })
+
+  },
+  onChange:function(val){
+    this.setData({
+      phone:val.detail
+    })
+  },
+  pay:function(){
+    this.setData({
+      show:true
+    })
+  },
+  onClose:function(){
+    this.setData({
+      show: false
+    })
   },
   getTechDetail:function(teacherId){
     var me=this;
