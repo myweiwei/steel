84d3a8427342252bcb54fe30353a472c01fe547e
xxfen <18270353826@139.com>
2021-04-24 20:05:24 +0800

    头像名称可上传修改
    头像名称可上传修改

--- miniprogram/pages/mine/mine.js
+++ miniprogram/pages/mine/mine.js
@@ -8,13 +8,15 @@ Page({
   data: {
     headIcon:'',
     name:'',
-
+    nameLen:4,
     userInfo:{},
     activeIndex:0,
     money:0,
     showPwd:'false',
     countList:{},
     isTeacher:0,
+    inputFocus:false,
+    disabled:true,
     isEnterprise:0
   },
   onTeacherRegister:function(){
@@ -54,14 +56,92 @@ Page({
     let me = this;
     app.wxRequest('get', '/personal/user', {}, function (res) {
       console.log(res.data)
+      let name=res.data.data.nickName;
+     var nameLen= me.strlen(name);
       me.setData({
+        nameLen:nameLen,
         headIcon:res.data.data.headIcon,
-        name:res.data.data.nickName,
+        name:name,
         isTeacher: res.data.data.isTeacher,
         isEnterprise: res.data.data.isEnterprise
       })
     }) 
   },
+  editHeadIconAndName: async function (arg) {
+    let me = this;
+
+    app.wxRequest('post', '/personal/user', arg, function (data) {
+      if (data.data.status != 200) {
+        wx.showToast({
+          title: data.data.msg,
+          icon: "none"
+        })
+      }
+      else {
+        wx.showToast({
+          title: '已提交',
+          icon: "success",
+          success: function (res) {
+            me.setData({
+              active: 2
+            })
+          }
+        });
+      }
+    })
+  },
+  onChangeInput:function(e){
+    let me = this;
+    var nameLen= me.strlen(e.detail.value);
+    console.log(nameLen);
+    this.setData({
+      nameValue: e.detail.value,
+      nameLen:nameLen
+    })
+    
+  },
+  /**
+   * 设置可编辑状态
+   */
+  setEdit:function(){
+    this.setData({
+      disabled: false,
+      inputFocus:true
+    })
+  },
+  /**
+   * 得到焦点
+   * @param {*} e 
+   */
+  onInputFocusGet:function(e){
+    //不显示编辑按钮
+    this.setData({
+      disabled: false
+    })
+  },
+  /**
+   * 失去焦点
+   * @param {*} e 
+   */
+  onInputFocusLose:function(e){
+    let me = this;
+    var nameLen= me.strlen(e.detail.value);
+    console.log(nameLen);
+    // this.setData({
+    //   nameValue: e.detail.value,
+    //   nameLen:nameLen
+    // })
+        //显示编辑按钮
+        this.setData({
+          disabled: true
+        })
+        var arg = {
+          nickName: e.detail.value,
+          headIcon: me.data.headIcon
+        }
+        me.editHeadIconAndName(arg);
+  },
+
   goMessage:function(){
     wx.navigateTo({
       url: '/pages/mine/message/message?followCount=' + this.data.countList.followCount + '&&supportCount=' + this.data.countList.supportCount + '&&commentCount=' + this.data.countList.commentCount
@@ -158,6 +238,69 @@ Page({
       show: true
     })
   },
+
+  selectImg:function(){
+    var me = this;
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success (res) {
+        // tempFilePath可以作为img标签的src属性显示图片
+        // const tempFilePaths = res.tempFilePaths.path
+        // console.log(res.tempFilePaths[0]);
+        me.addHead(res.tempFilePaths[0])
+      }
+    })
+  },
+  addHead:function(path){
+    var me = this;
+    wx.showLoading({
+      title: '上传中',
+    })
+    wx.uploadFile({
+      url: app.globalData.baseUrl + '/dynamic/fileupload',
+      filePath: path,
+      name: 'file',
+      formData: {
+      },
+      header: {
+        "Content-Type": "multipart/form-data",
+        'Authorization': app.globalData.token
+      },
+      success: function (res) {
+     var headIcon=   JSON.parse(res.data).data
+        me.setData({
+          headIcon: headIcon
+        });
+        console.log(headIcon);
+        var arg = {
+          nickName: me.data.name,
+          headIcon: headIcon
+        }
+        wx.hideLoading();
+        me.editHeadIconAndName(arg);
+      }, fail: function (err) {
+        console.log(err);
+      }
+    })
+  },
+    
+
+  strlen:function (str){  
+    var len = 0;  
+    for (var i=0; i<str.length; i++) {   
+     var c = str.charCodeAt(i);   
+    //单字节加1   
+     if ((c >= 0x0001 && c <= 0x007e) || (0xff60<=c && c<=0xff9f)) {   
+       len++;   
+     }   
+     else {   
+      len+=2.1;   
+     }   
+    }   
+    return len;  
+}  ,
   /**
    * 生命周期函数--监听页面加载
    */
